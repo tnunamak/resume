6461a55a579a2afec53ed3f40ee399fc111ef5e0
Tim Nunamaker <tim@timnunamaker.com>
2013-08-07 23:52:39 -0500

    Add ngFocus and ngBlur directives

---
+++ app/scripts/directives/ngFocusAndBlur.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var app = angular.module('resumeApp');
+
+app.directive('ngFocus', ['$parse', function($parse) {
+  return function(scope, element, attr) {
+    var fn = $parse(attr['ngFocus']);
+    element.bind('focus', function(event) {
+      scope.$apply(function() {
+        fn(scope, {$event:event});
+      });
+    });
+  }
+}]);
+
+app.directive('ngBlur', ['$parse', function($parse) {
+  return function(scope, element, attr) {
+    var fn = $parse(attr['ngBlur']);
+    element.bind('blur', function(event) {
+      scope.$apply(function() {
+        fn(scope, {$event:event});
+      });
+    });
+  }
+}]);
