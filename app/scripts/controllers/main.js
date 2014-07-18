c8398795047e63836c5641acb2c1312eca02c73d
Tim Nunamaker <tim@timnunamaker.com>
2014-07-17 21:41:29 -0500

    Content updates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,25 +16,34 @@ angular.module('resumeApp')
       phone: '210 · 281 · 1280',
       twitter: 'tnunamak',
       linkedIn: 'tnunamak',
-      summary: "I'm a developer in San Antonio with a background in full-stack Java web development. My expertise mostly revolves around Java but I have recently begun leaning more into JavaScript and tooling for it.",
+      summary: "Currently in Austin making web applications with JavaScript",
       experiences: [{
+        title: 'Software Engineer',
+        company: 'Bazaarvoice',
+        start: '8/13',
+        end: null,
+        current: true,
+        summary: 'I develop and maintain the UI side of the customer configuration app',
+        technologies: ['JavaScript', 'Backbone.js', 'AngularJS'],
+        responsibilities: "* Building the latest version of the tool clients use to configure the product, e.g.:\r\n\r\n    * **Wrote a saving abstraction** that auto-throttles and batches changes, broadcasts events to subscribers\r\n\r\n    * **Built a route-oriented search** mechanism that loads and scrolls to your result\r\n\r\n    * **\"Unforked\" a core dependency** that took refactoring changes from [backbone-forms](https://github.com/powmedia/backbone-forms) into files that could be layered on top of the library, to make it upgradeable\r\n\r\n    * **Manage the Grunt build** that I wrote to build an optimized project artifact\r\n\r\n    * **Implemented a testing strategy** tied to the build process\r\n\r\n* **Built a customizable JSON editor** as a personal-turned-production project. Interactively browse through, add or remove nested properties. Supports special rendering rules for specific documents. Documents may have customized JSON property labels, property display filtering and embedded forms for editing changes."
+      },{
         title: 'Senior Developer',
         company: 'HEB Grocery',
         start: '3/13',
-        end: null,
-        current: true,
-        summary: 'I am currently working on projects to improve the general capabilities of our development teams:',
+        end: '8/13',
+        current: false,
+        summary: 'Worked on projects to improve the general capabilities of our development teams:',
         technologies: ['Java', 'Spring', 'Gradle', 'AngularJS'],
-        responsibilities: "* **Rebuilt a large multi-project build process** with 20 interdependent modules. I designed and built a Gradle plugin for Oracle ATG E-commerce projects centered around mapping Gradle sub-projects to ATG modules. Performance  of the build reduced from a 10-45 minute execution time in Ant to 10 seconds-2 minutes in the rewrite.\r\n\r\n* **Bootstrapping AngularJS**. I wrote a proof-of-concept app on a modern JavaScript stack with Yeoman and centered around AngularJS. It demos unit testing Angular controllers and services, token based-authentication, and encapsulating heavy customizations to HTML via directives. This PoC effort started the current conversation about whether AngularJS and the single-page app paradigm has a place as a standard technology.\r\n\r\n* **Managing the transition process from CVS to Git**, including documentation, a support team, and training for project teams. This was identified as a necessary but unwieldy obstacle that I signed up for. Now in the pilot phase, new teams are requesting support from me on a daily basis.\r\n\r\n* **Bringing code reviews to the masses**. Implementing the peer code review process that I found success in trialing by catching bugs and bad code on a daily basis (see accounting project below)."
+        responsibilities: "* **Rebuilt a large multi-project build process** with 20 interdependent modules. I designed and built a Gradle plugin for Oracle ATG E-commerce projects centered around mapping Gradle sub-projects to ATG modules. Performance  of the build reduced from a 10-45 minute execution time in Ant to 10 seconds-2 minutes in the rewrite.\r\n\r\n* **Bootstrapping AngularJS**. I wrote a proof-of-concept app on a modern JavaScript stack with Yeoman and centered around AngularJS. It demos unit testing Angular controllers and services, token based-authentication, and encapsulating heavy customizations to HTML via directives. This PoC effort started the conversation about whether AngularJS and the single-page app paradigm has a place as a standard technology.\r\n\r\n* **Managing the transition process from CVS to Git**, including documentation, a support team, and training for project teams. This was identified as a necessary but unwieldy obstacle that I signed up for.\r\n\r\n* **Bringing code reviews to the masses**. Implementing the peer code review process that I found success in trialing by catching bugs and bad code on a daily basis (see accounting project below)."
       },{
         title: 'Developer',
         company: 'Contract and Side Jobs',
         start: '6/12',
-        end: null,
-        current: true,
+        end: '3/13',
+        current: false,
         summary: '',
         technologies: ['Android', 'AngularJS'],
-        responsibilities: "* **Shipping Android**: I am leading a team, for fun and learning, through building an Android app for generating drink recommendations. When the core features on the Kanban board are complete we will release it for public use and tailor future releases to user wants. (6\/13 - present) \r\n\r\n* **Built a single-page app with AngularJS\/Bootstrap\/PHP**: this lightweight product was for a customer to sell to realty service providers. Realtors gain access to videos of residential areas, accessed through the Vimeo API, for sharing with home-buyers. I worked on the AngularJS\/Bootstrap front-end in a small team under contract. (3\/13 - 2 months)\r\n\r\n* **Developed an apartment tenant-screening product** for a client to sell as a service. This Spring MVC app enabled leasing agents to run background checks on potential tenants. As one of two developers, my focus was on integrating with TransUnion and BDS services to look up applicant criminal and credit history, converting many shapes of XML, read over a socket, into useful Java objects. (6\/12 - 5 months)"
+        responsibilities: "* **Shipping Android**: I led a team, for fun and learning, through building an Android app for generating drink recommendations. A functional MVP, only missing recipe data, is complete.\r\n\r\n* **Built a single-page app with AngularJS\/Bootstrap\/PHP**: this lightweight product was for a customer to sell to realty service providers. Realtors gain access to videos of residential areas, accessed through the Vimeo API, for sharing with home-buyers. I worked on the AngularJS\/Bootstrap front-end in a small team under contract. (3\/13 - 2 months)\r\n\r\n* **Developed an apartment tenant-screening product** for a client to sell as a service. This Spring MVC app enabled leasing agents to run background checks on potential tenants. As one of two developers, my focus was on integrating with TransUnion and BDS services to look up applicant criminal and credit history, converting many shapes of XML, read over a socket, into useful Java objects. (6\/12 - 5 months)"
       },{
         title: 'Developer',
         company: 'HEB Grocery',
@@ -64,14 +73,21 @@ angular.module('resumeApp')
         achievements: "* Honors in Computer Science ([Thesis: Cycles in Metabolic Networks](http:\/\/digitalcommons.trinity.edu\/compsci_honors\/25\/))  \r\n* Received Meritorious award (top 13%) for international 2009 COMAP math modeling competition\r\n* David Swalm scholarship awarded for Engineering, 2008  \r\n* HHMI-funded for math and biology summer research, 2007. See aforementioned thesis.\r\n* Engineering program was multidisciplinary, with a strong emphasis on design"
       }],
       projects: [{
+          name: 'Robits',
+          start: '6/14',
+          end: '7/14',
+          current: false,
+          technologies: ['JavaScript', 'Phaser.io', 'Node.js', 'Socket.io'],
+          homepage: "http://github.com/Atlas-/robits",
+          description: "A multiplayer JavaScript game loosely based on the board game RoboRally. The outcome of a self-organized game jam."
+      },{
         name: 'Technology Radar',
         start: '7/13',
-        end: null,
-        current: true,
+        end: '7/13',
+        current: false,
         technologies: ['AngularJS', 'D3.js'],
         homepage: "http://github.com/tnunamak/tech-radar",
-        description: "[The radar](http:\/\/www.thoughtworks.com\/insights) is a diagram that groups tools, techniques, languages\/frameworks and platforms into the categories adopt, trial, assess and hold. This implementation is intended to help developers, teams or organizations maintain a visual map of their toolbox.\r\n\r\nThe application currently utilizes D3 to render the diagram from a data structure that contains all of the technologies. Users can add and remove technologies and changes are cached in the browser\'s local storage. I will soon connect it to a RESTful backend to manage user data in a mongo database.",
-        contribution: "This is a personal, open project that I maintain."
+        description: "[The radar](http:\/\/www.thoughtworks.com\/insights) is a diagram that groups tools, techniques, languages\/frameworks and platforms into the categories adopt, trial, assess and hold. This implementation is intended to help developers, teams or organizations maintain a visual map of their toolbox.\r\n\r\nThe application currently utilizes D3 to render the diagram from a data structure that contains all of the technologies. Users can add and remove technologies and changes are cached in the browser\'s local storage."
       }]
     };
   });
